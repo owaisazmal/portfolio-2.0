c194b9c2df1d8c135022724e17eeebb47c8212cd
Owais <owais.khan.244@my.csun.edu>
2025-05-23 16:11:22 -0700

    2280063557a1179ce9e22b2dadecb0e14286dfab.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-//const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     output: 'export',
-    basePath: '/portfolio-2.0-main',
-    assetPrefix: '/portfolio-2.0-main/',
+    basePath: isProd ? '/portfolio-2.0-main' : '',
+    assetPrefix: isProd ? '/portfolio-2.0-main/' : '',
     images: {
         unoptimized: true,
     },
